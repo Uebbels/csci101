a0d91243aba9ce42c3af915db99c56c5023eb7fb
Brendan Uebelhoer <brendanuebelhoer@gmail.com>
2019-11-21 20:46:58 -0700

    inmpements read unemp function

--- Week12/Lab10-economics101.py
+++ Week12/Lab10-economics101.py
@@ -1,13 +1,13 @@
 # Brendan Uebelhoer
 # ​CSCI 101 – Section A
 # Python Lab 10
+import csv
 
 def get_gdp_data(fileName, year):
     gdp = []
     with open(fileName, 'r') as gdpfile:
         gdpcontents =  gdpfile.readlines()
     for i in gdpcontents:
-        #print(i[0:4], year)
         if i[0:4] == str(year):
             yeardata = i.split()
             gdp.append(yeardata[1])
@@ -15,7 +15,12 @@ def get_gdp_data(fileName, year):
 
 def get_unemp_data(fileName, year):
     unemp = []
-    
+    with open(fileName, 'r') as csvfile:
+        grades_reader = csv.reader(csvfile)
+        for i in grades_reader:
+            if i[0] == str(year):
+                unemp = i[1:]
+    return unemp
 
 yearvalid = False
 
@@ -34,5 +39,5 @@ while yearvalid == False:
         print('invalid year, please try again')
 
 print(get_gdp_data(GDPfilename,year))
-
+print(get_unemp_data(UEfilename, year))
 
